72fc5b33bec4e2a3cadce8be8faad21f0fd5e453
mohini2023 <mohinimishra759@gmail.com>
2023-09-16 20:35:48 +0530

    16/9/2023

--- src/Files/Card.js
+++ src/Files/Card.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Details from './Details';
 
-function Card({ recipe, onItemClick }) {
-  return (
-    <div className="row">
-      {recipe.map((item, index) => (
-        <div key={index} className="col-md-3 mb-3">
-          <div className="card">
-            <img
-              src={item.recipe.image}
-              className="card-img-top"
-              alt={item.recipe.label}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{item.recipe.label}</h5>
-              <button
-                className="btn btn-primary"
-                onClick={() => onItemClick(item)}
-              >
-                View Details
-              </button>
+function Card({ recipe }) {
+  const [show, setShow] = useState(false);
+  const [recipeItem, setItem] = useState(null);
+
+  const handleCardClick = (item) => {
+    setShow(true);
+    setItem(item);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  if (recipe && recipe.length > 0) {
+    return (
+      <div className="row">
+        {recipe.map((item) => (
+          <div className="col-md-3" key={item.recipe.label}>
+            <div
+              className="card"
+              onClick={() => handleCardClick(item)}
+            >
+              <img src={item.recipe.image} alt="" />
+              <div className="details">
+                <h3 className="title">Name: {item.recipe.label}</h3>
+                <h4 className="diet">Diet Label: {item.recipe.dietLabels}</h4>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+        {show && <Details show={show} item={recipeItem} onClose={handleClose} />}
+      </div>
+    );
+  }
+
+  return null;
 }
 
 export default Card;
